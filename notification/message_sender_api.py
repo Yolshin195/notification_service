6b297fab3e7583119ed328d47b31e5a37c0f53e6
Yolshin195 <elshin195@gmail.com>
2024-01-17 01:50:30 +0700

    added MessageSenderApi

--- notification/message_sender_api.py
+++ notification/message_sender_api.py
@@ -1,29 +1,48 @@
-import requests
+from typing import TypedDict, Unpack
+from urllib import request, error
+from logging import getLogger
+import json
+
+from django.conf import settings
+
+logger = getLogger(__name__)
+
+
+class Message(TypedDict):
+    id: int
+    phone: int
+    text: str
 
 
 class MessageSenderApi:
     def __init__(self, path, token):
         self.path = path
         self.headers = {
-            'content-type': 'application/json',
+            'Content-Type': 'application/json',
             'Authorization': f'Bearer {token}'
         }
+        self.method = 'POST'
 
-    def __call__(self, message_id: int, phone: int, text: str) -> bool:
+    def get_url(self, message_id: int):
+        return f"{self.path}/{message_id}"
+
+    def __call__(self, **body: Unpack[Message]) -> bool:
         try:
-            body = {
-                'id': message_id,
-                'phone': phone,
-                'text': text
-            }
-            response = requests.post(f"{self.path}/{message_id}", json=body, headers=self.headers)
-            return True if response.json()['message'] == 'OK' else False
-        except:
+            data = json.dumps(body).encode('utf-8')
+            req = request.Request(self.get_url(body['id']), data=data, headers=self.headers, method=self.method)
+            with request.urlopen(req) as response:
+                response_data = json.loads(response.read().decode('utf-8'))
+                return True if response_data.get('message') == 'OK' else False
+        except error.URLError as e:
+            logger.error(f"Request failed: {e}")
+            return False
+        except json.JSONDecodeError as e:
+            logger.error(f"Failed to parse JSON response: {e}")
             return False
 
 
-message_sender_api = MessageSenderApi(path="https://probe.fbrq.cloud/v1/send", token="your token")
+message_sender_api = MessageSenderApi(path=settings.MESSAGE_SENDER_API_PATH, token=settings.MESSAGE_SENDER_API_TOKEN)
 
 if __name__ == "__main__":
-    r = message_sender_api(1, 79834567233, 'Test')
+    r = message_sender_api(id=1, phone=79834567233, text='Test')
     print(r)
